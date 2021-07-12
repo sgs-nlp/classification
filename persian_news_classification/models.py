f7c4258ef403bf683f952288e9f6a5a3927ab4b0
Navid <n.soleymani@pnr.ir>
2021-07-12 14:08:12 +0430

    init database model

--- persian_news_classification/models.py
+++ persian_news_classification/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(
+        max_length=128,
+    )
+
+
+class News(models.Model):
+    titr = models.CharField(
+        max_length=512,
+    )
+    context = models.TextField(
+
+    )
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        blank=False,
+        null=False,
+    )
+    vector = models.JSONField(
+
+    )
+    refrence = models.CharField(
+        max_length=128,
+    )
