adbca636327f5285fbefecbfb8e5f6beb4bf27d1
Navid <n.soleymani@pnr.ir>
2021-07-12 17:14:59 +0430

    create views (and urls) for prerequisites handling

--- persian_news_classification/views.py
+++ persian_news_classification/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpRequest, JsonResponse
+from .dataset2database import add2database
 
-# Create your views here.
+
+def prerequisites(request: HttpRequest):
+    from pathlib import Path
+    file_name = Path('statics_files', 'HamshahriData.xlsx')
+    add2database(file_name)
+    return redirect('persian_news_classification:index')
+
+
+def index(request: HttpRequest):
+    return render(request, 'test.html', context={})
