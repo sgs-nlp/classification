8b5e4ae5b85660cb94424967f8e6d23253e1871d
Navid <n.soleymani@pnr.ir>
2021-07-19 19:41:16 +0430

    classification - create controller

--- persian_news_classification/controller.py
+++ persian_news_classification/controller.py
@@ -14,6 +14,41 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.neural_network import MLPClassifier
 from sklearn import svm
+import numpy as np
+
+from nvd.normalizer import matrix_scale_matrix
+from nvd.embedding import GDoc2Vec
+
+
+def classification():
+    reference_title = 'staticfiles/HamshahriData.xlsx'
+
+    # load categories on database
+    # ->
+    _category_list = Category.objects.filter(reference__title=reference_title).all()
+    category_list = []
+    for category in _category_list:
+        category_list.append(category.title_code)
+    # <-
+    # load on database
+    # ->
+    news = News.objects.filter(reference__title=reference_title).all()
+    # <-
+    # pandas data frame
+    # ->
+    x_data = []
+    y_data = []
+    for itm in news:
+        tmp = [int(itm.category.title_code)]
+        for word in itm.titr_words_without_stopword_code[0]:
+            tmp.append(int(word))
+        x_data.append(tmp)
+        y_data.append(category_list.index(itm.category.title_code))
+    gensim_d2v_model = GDoc2Vec(x_data)
+    x_data = gensim_d2v_model.vectors
+    print(f"multinomial naive baise model error: {multinomial_naive_baise_model(x_data, y_data)}")
+    print(f'mlp model error: {multi_layer_perseptron_model(x_data, y_data)}')
+    print(f'svm model error: {suport_vector_machine_model(x_data, y_data)}')
 
 
 def classification_2():
@@ -38,6 +73,7 @@ def classification_2():
             'data': f'{itm.titr_string_code} {itm.content_string_code}',
             'flag': category_list.index(itm.category.title_code)
         })
+    # print(_data)
     training_data = pandas.DataFrame(_data, columns=['data', 'flag'])
     # print(data)
     # <-
@@ -57,7 +93,6 @@ def classification_2():
     # GET VECTOR COUNT
     count_vect = CountVectorizer()
     X_train_counts = count_vect.fit_transform(training_data.data)
-
     # SAVE WORD VECTOR
     pickle.dump(count_vect.vocabulary_, open("count_vector.pkl", "wb"))
 
@@ -66,15 +101,14 @@ def classification_2():
     # TRANSFORM WORD VECTOR TO TF IDF
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-
     # SAVE TF-IDF
     pickle.dump(tfidf_transformer, open("tfidf.pkl", "wb"))
 
     # %%
 
-    # print(
-    #     f"multinomial naive baise model error: "
-    #     f"{multinomial_naive_baise_model(X_train_tfidf, training_data, category_list)}")
+    print(
+        f"multinomial naive baise model error: "
+        f"{multinomial_naive_baise_model(X_train_tfidf, training_data, category_list)}")
 
     # confusion_mat = confusion_matrix(y_test, predicted)
     # print(confusion_mat)
@@ -83,62 +117,55 @@ def classification_2():
     # print(f'svm model error: {svm_model(X_train_tfidf, training_data, category_list)}')
 
 
-def multinomial_naive_baise_model(X_train_tfidf, training_data, category_list):
-    # Multinomial Naive Bayes
-
-    X_train, X_test, y_train, y_test = train_test_split(X_train_tfidf, training_data.flag, test_size=0.25,
-                                                        random_state=42)
-    clf = MultinomialNB().fit(X_train, y_train)
-
-    pickle.dump(clf, open("nb_model.pkl", "wb"))
-
-    predicted = clf.predict(X_test)
+def multinomial_naive_baise_model(x_data, y_data):
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.15, random_state=42)
+    nb_model = MultinomialNB().fit(x_train, y_train)
+    predicted = nb_model.predict(x_test)
     result_bayes = pandas.DataFrame({'true_labels': y_test, 'predicted_labels': predicted})
-    result_bayes.to_csv('res_bayes.csv', sep=',')
-
+    print(result_bayes)
     number_of_err = 0
     total = 0
     for predicted_item, result in zip(predicted, y_test):
         total += 1
-        if predicted_item != category_list[result]:
+        if predicted_item != result:
             number_of_err += 1
-    return (number_of_err / total) * 100
+    return 100 - ((number_of_err / total) * 100)
 
 
-def mlp_model(X_train_tfidf, training_data, category_list):
-    clf_neural = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
-    X_train, X_test, y_train, y_test = train_test_split(X_train_tfidf, training_data.flag, test_size=0.25,
+def multi_layer_perseptron_model(x_data, y_data):
+    mlp_model = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.25,
                                                         random_state=42)
-    clf_neural.fit(X_train, y_train)
-    pickle.dump(clf_neural, open("softmax.pkl", "wb"))
-    predicted = clf_neural.predict(X_test)
+    mlp_model.fit(X_train, y_train)
+    # pickle.dump(mlp_model, open("softmax.pkl", "wb"))
+    predicted = mlp_model.predict(X_test)
     result_softmax = pandas.DataFrame({'true_labels': y_test, 'predicted_labels': predicted})
-    result_softmax.to_csv('res_softmax.csv', sep=',')
-
+    # result_softmax.to_csv('res_softmax.csv', sep=',')
+    print(result_softmax)
     number_of_err = 0
     total = 0
     for predicted_item, result in zip(predicted, y_test):
         total += 1
-        if predicted_item != category_list[result]:
+        if predicted_item != result:
             number_of_err += 1
-    return (number_of_err / total) * 100
+    return 100 - ((number_of_err / total) * 100)
 
 
-def svm_model(X_train_tfidf, training_data, category_list):
-    clf_svm = svm.LinearSVC()
-    X_train, X_test, y_train, y_test = train_test_split(X_train_tfidf, training_data.flag, test_size=0.25,
+def suport_vector_machine_model(x_data, y_data):
+    svm_model = svm.LinearSVC()
+    x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, test_size=0.25,
                                                         random_state=42)
-    clf_svm.fit(X_train_tfidf, training_data.flag)
-    pickle.dump(clf_svm, open("svm.pkl", "wb"))
+    svm_model.fit(x_train, y_train)
+    # pickle.dump(svm_model, open("svm.pkl", "wb"))
 
-    predicted = clf_svm.predict(X_test)
+    predicted = svm_model.predict(x_test)
     result_svm = pandas.DataFrame({'true_labels': y_test, 'predicted_labels': predicted})
-    result_svm.to_csv('res_svm.csv', sep=',')
-
+    # result_svm.to_csv('res_svm.csv', sep=',')
+    print(result_svm)
     number_of_err = 0
     total = 0
     for predicted_item, result in zip(predicted, y_test):
         total += 1
-        if predicted_item != category_list[result]:
+        if predicted_item != result:
             number_of_err += 1
-    return (number_of_err / total) * 100
+    return 100 - ((number_of_err / total) * 100)
