83d855b37067e1f5cbcefd8c942fd717de881b33
Navid <n.soleymani@pnr.ir>
2021-07-31 20:48:37 +0430

    todo

--- nvd/measure.py
+++ nvd/measure.py
@@ -30,16 +30,19 @@ def accuracy(false_negatives, true_positives, true_negatives, false_positives):
 
 
 def balanced_accuracy(false_negatives, true_positives, true_negatives, false_positives):
+    # todo
     tpr = true_positives / (true_positives + false_negatives)
     tnr = true_negatives / (true_negatives + false_positives)
     return (tpr + tnr) / 2
 
 
 def predicted_positive_condition_rate(false_negatives, true_positives, true_negatives, false_positives):
+    # todo
     return (true_positives + false_positives) / (false_negatives + true_positives + true_negatives + false_positives)
 
 
 def f_beta(false_negatives, true_positives, true_negatives, false_positives, beta=1):
+    # todo
     precision_score = precision(false_negatives, true_positives, true_negatives, false_positives)
     recall_score = recall(false_negatives, true_positives, true_negatives, false_positives)
     return (1 + beta ** 2)((precision_score * recall_score) / ((beta ** 2) * precision_score + recall_score))
