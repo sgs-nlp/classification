168e43823a7ed76b115eaaa3d0337dd3205159fb
Navid <n.soleymani@pnr.ir>
2021-08-01 20:34:05 +0430

    add logging and create pickle files

--- persian_news_classification/dataset2database.py
+++ persian_news_classification/dataset2database.py
@@ -1,10 +1,12 @@
 import json
 import logging
 import openpyxl
+import pickle
+import os
 
 from nvd.pre_processing import normilizer, tokenizer, without_stopword
 
-from .models import Reference, add_reference, add_category, add_news, add_stopword, add_word
+from .models import Reference, add_reference, add_category, add_news, add_stopword, add_word, File
 
 
 def add2database(file_name: str) -> int:
@@ -13,20 +15,30 @@ def add2database(file_name: str) -> int:
         logging.info('The desired datset is available in the database.')
         return reference_id
     logging.info('Started storing persian symbols in the database.')
-    from nvd.symbols import LIST as PERSIAN_SYMBOLS
-    for word in PERSIAN_SYMBOLS:
-        add_word(word)
+    if not os.path.isfile('uploads/PERSIAN_SYMBOLS.pkl'):
+        from nvd.symbols import LIST as PERSIAN_SYMBOLS
+        logging.info('Started persian symbols pkl file craeted.')
+        with open('uploads/PERSIAN_SYMBOLS.pkl', 'ab') as pkl_file:
+            pickle.dump(PERSIAN_SYMBOLS, pkl_file)
+        logging.info('create persian symbols pkl file complate.')
+        for word in PERSIAN_SYMBOLS:
+            add_word(word)
     logging.info('Persian symbols storage in the database is complete.')
     reference_id = add_reference(file_name)
     logging.info('Started stopwords list loading.')
-    stop_word_list_name = f'{file_name}.persian.stopword.json'
-    with open(stop_word_list_name, 'r', encoding='utf-8') as file:
-        stopwords_list = file.read()
-        stopwords_list = json.loads(stopwords_list)
-    logging.info('Stopwords list loaded.')
-    logging.info('Started storing persian stopwords list in the database.')
-    for word in stopwords_list:
-        add_stopword(word, reference_id)
+    if not os.path.isfile('uploads/stopwords_list.pkl'):
+        stop_word_list_name = f'{file_name}.persian.stopword.json'
+        with open(stop_word_list_name, 'r', encoding='utf-8') as file:
+            stopwords_list = file.read()
+            stopwords_list = json.loads(stopwords_list)
+        logging.info('Stopwords list loaded.')
+        logging.info('Started storing persian stopwords list in the database.')
+        for word in stopwords_list:
+            add_stopword(word, reference_id)
+        logging.info('Started stopwords pkl file craeted.')
+        with open('uploads/stopwords_list.pkl', 'ab') as pkl_file:
+            pickle.dump(stopwords_list, pkl_file)
+        logging.info('create stopwords pkl file complate.')
     logging.info('Persian stopwords storage in the database is complete.')
     logging.info('Started news file loading.')
     wb_obj = openpyxl.load_workbook(file_name)
@@ -70,4 +82,14 @@ def add2database(file_name: str) -> int:
         )
         logging.info(f'The number {row_number} news item storage in the database is complete.')
         row_number += 1
+    logging.info('Started categories pkl file craeted.')
+    if not os.path.isfile('uploads/categories.pkl'):
+        from .models import Category
+        _categories = Category.objects.filter(reference_id=reference_id).all()
+        categories = []
+        for cat in _categories:
+            categories.append(dict(cat))
+        with open('uploads/categories.pkl', 'ab') as pkl_file:
+            pickle.dump(categories, pkl_file)
+    logging.info('create categories pkl file complate.')
     return reference_id
