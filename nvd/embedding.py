0df0f12ad78dd6c0c6cd1c5a8fae292144069760
Navid <n.soleymani@pnr.ir>
2021-08-02 21:48:52 +0430

    tuning

--- nvd/embedding.py
+++ nvd/embedding.py
@@ -3,6 +3,7 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from gensim.test.utils import get_tmpfile
 from os import path
 from nvd.normalizer import matrix_scale_matrix
+import numpy
 
 
 class GDoc2Vec:
@@ -10,19 +11,14 @@ class GDoc2Vec:
             self,
             data=None,
             file_name='model.gensim.d2v',
-            vector_size=100,
-            dm_mean=None,
+            vector_size=100,  # todo 2 ** 8,
+            dm_mean=1,
             dm=1,
-            dbow_words=0,
-            dm_concat=0,
+            dbow_words=1,
+            dm_concat=1,
             dm_tag_count=1,
-            dv=None,
-            dv_mapfile=None,
-            comment=None,
-            trim_rule=None,
-            callbacks=(),
-            window=5,
-            epochs=10
+            window=3,  # todo 2**4,
+            epochs=5  # todo 2**10
     ):
         self.vector_size = vector_size
         self.dm_mean = dm_mean
@@ -30,11 +26,6 @@ class GDoc2Vec:
         self.dbow_words = dbow_words
         self.dm_concat = dm_concat
         self.dm_tag_count = dm_tag_count
-        self.dv = dv
-        self.dv_mapfile = dv_mapfile
-        self.comment = comment
-        self.trim_rule = trim_rule
-        self.callbacks = callbacks
         self.window = window
         self.epochs = epochs
         if data is not None:
@@ -47,11 +38,6 @@ class GDoc2Vec:
                 dbow_words=self.dbow_words,
                 dm_concat=self.dm_concat,
                 dm_tag_count=self.dm_tag_count,
-                dv=self.dv,
-                dv_mapfile=self.dv_mapfile,
-                comment=self.comment,
-                trim_rule=self.trim_rule,
-                callbacks=self.callbacks,
                 window=self.window,
                 epochs=self.epochs,
             )
@@ -64,7 +50,7 @@ class GDoc2Vec:
 
     @property
     def vectors(self):
-        return matrix_scale_matrix(self.model.docvecs.vectors)
+        return self.model.docvecs.vectors
 
     def d2v(self, document):
         pass
