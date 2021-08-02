6a24406d60fc48155700f6845c70d9734dab79ba
Navid <n.soleymani@pnr.ir>
2021-08-02 21:54:01 +0430

    add classification type

--- persian_news_classification/controller.py
+++ persian_news_classification/controller.py
@@ -230,3 +230,245 @@ def classification(reference_title: str = 'staticfiles/HamshahriData.xlsx'):
 #         if predicted_item != result:
 #             number_of_err += 1
 #     return 100 - ((number_of_err / total) * 100)
+
+
+class Classification:
+    def __init__(self, x_data, y_data, categories_list):
+        self.x_data = x_data
+        self.y_data = y_data
+        self.categories_list = categories_list
+
+    _x_train = None
+    _x_test = None
+    _y_train = None
+    _y_test = None
+
+    @property
+    def x_train(self):
+        if self._x_train is not None:
+            return self._x_train
+        self._x_train, self._x_test, self._y_train, self._y_test = train_test_split(self.x_data, self.y_data,
+                                                                                    test_size=0.15, random_state=42)
+        return self._x_train
+
+    @property
+    def x_test(self):
+        if self._x_test is not None:
+            return self._x_test
+        self._x_train, self._x_test, self._y_train, self._y_test = train_test_split(self.x_data, self.y_data,
+                                                                                    test_size=0.15, random_state=42)
+        return self._x_test
+
+    @property
+    def y_train(self):
+        if self._y_train is not None:
+            return self._y_train
+        self._x_train, self._x_test, self._y_train, self._y_test = train_test_split(self.x_data, self.y_data,
+                                                                                    test_size=0.15, random_state=42)
+        return self._y_train
+
+    @property
+    def y_test(self):
+        if self._y_test is not None:
+            return self._y_test
+        self._x_train, self._x_test, self._y_train, self._y_test = train_test_split(self.x_data, self.y_data,
+                                                                                    test_size=0.15, random_state=42)
+        return self._y_test
+
+    _svm = None
+
+    @property
+    def svm(self):
+        if self._svm is not None:
+            return self._svm
+        self._svm = SVM(self.x_train, self.x_test, self.y_train, self.y_test, self.categories_list)
+        return self._svm
+
+    _mlp = None
+
+    @property
+    def mlp(self):
+        if self._mlp is not None:
+            return self._mlp
+        self._mlp = SVM(self.x_train, self.x_test, self.y_train, self.y_test, self.categories_list)
+        return self._mlp
+
+
+class SVM:
+    def __init__(self, x_train, x_test, y_train, y_test, categories_list):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.categories_list = categories_list
+
+    _model = None
+
+    @property
+    def model(self):
+        if self._model is not None:
+            return self._model
+        self._model = support_vector_machine.LinearSVC()
+        self._model.fit(self.x_train, self.y_train)
+        return self._model
+
+    _fns = None
+    _tps = None
+    _tns = None
+    _fps = None
+
+    @property
+    def fns(self):
+        if self._fns is not None:
+            return self._fns
+        predicted = self.model.predict(self.x_test)
+        self._fns, self._tps, self._tns, self._fps = true_or_false(predicted, self.y_test, self.categories_list)
+        return self._fns
+
+    @property
+    def tps(self):
+        if self._tps is not None:
+            return self._tps
+        predicted = self.model.predict(self.x_test)
+        self._fns, self._tps, self._tns, self._fps = true_or_false(predicted, self.y_test, self.categories_list)
+        return self._tps
+
+    @property
+    def tns(self):
+        if self._tns is not None:
+            return self._tns
+        predicted = self.model.predict(self.x_test)
+        self._fns, self._tps, self._tns, self._fps = true_or_false(predicted, self.y_test, self.categories_list)
+        return self._tns
+
+    @property
+    def fps(self):
+        if self._fps is not None:
+            return self._fps
+        predicted = self.model.predict(self.x_test)
+        self._fns, self._tps, self._tns, self._fps = true_or_false(predicted, self.y_test, self.categories_list)
+        return self._fps
+
+    _precision_score = None
+
+    @property
+    def precision_score(self):
+        if self._precision_score is not None:
+            return self._precision_score
+        self._precision_score = precision(true_positives=self.tps, false_positives=self.fps)
+        return self._precision_score
+
+    _recall_score = None
+
+    @property
+    def recall_score(self):
+        if self._recall_score is not None:
+            return self._recall_score
+        self._recall_score = recall(false_negatives=self.fns, true_positives=self.tps)
+        return self._recall_score
+
+    _accuracy_score = None
+
+    @property
+    def accuracy_score(self):
+        if self._accuracy_score is not None:
+            return self._accuracy_score
+        self._accuracy_score = accuracy(false_negatives=self.fns, true_positives=self.tps, true_negatives=self.tns,
+                                        false_positives=self.fps)
+        return self._accuracy_score
+
+        # if 'svm' in models_name_list:
+        #     model = support_vector_machine.LinearSVC()
+        #     model.fit(x_train, y_train)
+        #     predicted = svm_model.predict(x_test)
+        #     fns, tps, tns, fps = true_or_false(predicted, y_test, categories_list)
+        #     if 'precision' in measures_name_list:
+        #         per_svm = precision(true_positives=tps, false_positives=fps)
+        #     if 'recall' in measures_name_list:
+        #         rec_svm = recall(false_negatives=fns, true_positives=tps)
+        #     if 'accuracy' in measures_name_list:
+        #         acc_svm = accuracy(false_negatives=fns, true_positives=tps, true_negatives=tns, false_positives=fps)
+
+
+class MLP:
+    def __init__(self, x_train, x_test, y_train, y_test, categories_list):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.categories_list = categories_list
+
+    _model = None
+
+    @property
+    def model(self):
+        if self._model is not None:
+            return self._model
+        self._model = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1)
+        self._model.fit(self.x_train, self.y_train)
+        return self._model
+
+    _fns = None
+    _tps = None
+    _tns = None
+    _fps = None
+
+    @property
+    def fns(self):
+        if self._fns is not None:
+            return self._fns
+        predicted = self.model.predict(self.x_test)
+        self._fns, self._tps, self._tns, self._fps = true_or_false(predicted, self.y_test, self.categories_list)
+        return self._fns
+
+    @property
+    def tps(self):
+        if self._tps is not None:
+            return self._tps
+        predicted = self.model.predict(self.x_test)
+        self._fns, self._tps, self._tns, self._fps = true_or_false(predicted, self.y_test, self.categories_list)
+        return self._tps
+
+    @property
+    def tns(self):
+        if self._tns is not None:
+            return self._tns
+        predicted = self.model.predict(self.x_test)
+        self._fns, self._tps, self._tns, self._fps = true_or_false(predicted, self.y_test, self.categories_list)
+        return self._tns
+
+    @property
+    def fps(self):
+        if self._fps is not None:
+            return self._fps
+        predicted = self.model.predict(self.x_test)
+        self._fns, self._tps, self._tns, self._fps = true_or_false(predicted, self.y_test, self.categories_list)
+        return self._fps
+
+    _precision_score = None
+
+    @property
+    def precision_score(self):
+        if self._precision_score is not None:
+            return self._precision_score
+        self._precision_score = precision(true_positives=self.tps, false_positives=self.fps)
+        return self._precision_score
+
+    _recall_score = None
+
+    @property
+    def recall_score(self):
+        if self._recall_score is not None:
+            return self._recall_score
+        self._recall_score = recall(false_negatives=self.fns, true_positives=self.tps)
+        return self._recall_score
+
+    _accuracy_score = None
+
+    @property
+    def accuracy_score(self):
+        if self._accuracy_score is not None:
+            return self._accuracy_score
+        self._accuracy_score = accuracy(false_negatives=self.fns, true_positives=self.tps, true_negatives=self.tns,
+                                        false_positives=self.fps)
+        return self._accuracy_score
