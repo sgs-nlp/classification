ddfb73ee75e35f6c27cd179f1fa3ef4a29025ee7
Navid <n.soleymani@pnr.ir>
2021-08-03 20:42:44 +0430

    commenting

--- persian_news_classification/controller.py
+++ persian_news_classification/controller.py
@@ -10,7 +10,7 @@ from sklearn import svm as support_vector_machine
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.neural_network import MLPClassifier
 
-from .models import News, StopWord, Category, Word
+from .models import News, StopWord, Category, Word, code2word
 
 from nvd.normalizer import matrix_scale_matrix
 from nvd.embedding import GDoc2Vec, BOWDoc2vec, OneHotDoc2vec
@@ -29,35 +29,45 @@ def classification(reference_title: str = 'staticfiles/HamshahriData.xlsx'):
         category_list.append(category.title_code)
         category_index_list.append(i)
         i += 1
-
-    # with open('uploads/categories.pkl', 'rb') as pkl_file:
-    #     categories = pickle.load(pkl_file)
-    # print(categories)
-
     # <-
+
     # load on database
     # ->
     news = News.objects.filter(reference__title=reference_title).all()
     # <-
-    # pandas data frame
+
+    # data -> input: x_data, output: y_data
     # ->
     x_data = []
     y_data = []
     for itm in news:
-        tmp = [str(itm.category.title_code)]
-        for word in itm.titr_words_without_stopword_code[0]:
+        tmp = []
+        for word in itm.content_words_without_stopword_code[0]:
             tmp.append(str(word))
         x_data.append(tmp)
         y_data.append(itm.category.pk)
+    # <-
 
+    # create word vector with gensim w2v
+    # ->
     gensim_d2v_model = GDoc2Vec(x_data)
-    x_data_v = gensim_d2v_model.vectors
-    g_w2v_classifier = Classification(x_data_v, y_data, category_index_list)
+    x_data2gv = gensim_d2v_model.vectors
+    # <-
 
+    # create word vector with one-hot encoding
+    # ->
     dictionary = Word.objects.all()
     ohe_d2v_model = OneHotDoc2vec(x_data, dictionary)
-    x_data_v = ohe_d2v_model.vectors
-    o_w2v_classifier = Classification(x_data_v, y_data, category_index_list)
+    x_data2ov = ohe_d2v_model.vectors
+    # <-
+
+    # classification
+    # ->
+    g_w2v_classifier = Classification(x_data2gv, y_data, category_index_list)
+    o_w2v_classifier = Classification(x_data2ov, y_data, category_index_list)
+
+    # <-
+    # set template parameter
     res = {
         'word2vec_word_embeding__svm__precision_score': g_w2v_classifier.svm.precision_score,
         'word2vec_word_embeding__svm__recall_score': g_w2v_classifier.svm.recall_score,
@@ -83,4 +93,5 @@ def classification(reference_title: str = 'staticfiles/HamshahriData.xlsx'):
         'one_hot_encoding_word_embeding__mnb__recall_score': o_w2v_classifier.mnb.recall_score,
         'one_hot_encoding_word_embeding__mnb__accuracy_score': o_w2v_classifier.mnb.accuracy_score,
     }
+    # <-
     return res
