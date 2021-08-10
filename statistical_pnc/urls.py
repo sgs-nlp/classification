28f8f2f948637d69288ea10af20f7d8f9e33e541
Navid <n.soleymani@pnr.ir>
2021-08-10 19:10:56 +0430

    create classification path

--- statistical_pnc/urls.py
+++ statistical_pnc/urls.py
@@ -1,8 +1,9 @@
-from .views import index, prerequisites
+from .views import index, prerequisites, classification
 from django.urls import path, include
 
 app_name = 'statistical_pnc'
 urlpatterns = [
-    path('', prerequisites),
+    path('', prerequisites, name='prerequisites'),
     path('index', index, name='index'),
+    path('classification', classification, name='classification'),
 ]
