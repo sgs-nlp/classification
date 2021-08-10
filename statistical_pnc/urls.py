f948126296f57a333f5b4f47dc0f5e3555308235
Navid <n.soleymani@pnr.ir>
2021-08-10 23:17:41 +0430

    add pre processing path

--- statistical_pnc/urls.py
+++ statistical_pnc/urls.py
@@ -1,9 +1,10 @@
-from .views import index, prerequisites, classification
+from .views import index, sample, classification, preprocessing
 from django.urls import path, include
 
 app_name = 'statistical_pnc'
 urlpatterns = [
-    path('', prerequisites, name='prerequisites'),
-    path('index', index, name='index'),
+    path('', index, name='index'),
+    path('sample', sample, name='sample'),
     path('classification', classification, name='classification'),
+    path('preprocessing', preprocessing, name='preprocessing'),
 ]
