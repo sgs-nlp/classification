340b8aecec21e4266abfc4274296fd8fed6c9b47
nvd <navidsoleymani@ymail.com>
2021-08-14 22:36:52 +0430

    create news classification

--- statistical_pnc/controller.py
+++ statistical_pnc/controller.py
@@ -12,7 +12,29 @@ def prerequisites():
     corpus_file_path = Path('staticfiles', 'HamshahriData.xlsx')
     add2database(corpus_file_path)
     logging.info('Data storage in the database is complete.')
-    logging.info('Started classification analys.')
-    # clss = classification()
-    logging.info('Classification analys complate.')
-    return
+
+
+def news_classification(reference: Reference, content: str, titr: str = None) -> Category:
+    news = news2db(
+        content_string=content,
+        titr_string=titr,
+    )
+    # news = News.objects.get(pk=3)
+    news_vector = news2vector(news)
+    cats_list = categories_list(reference=reference, vector=True)
+    print(cats_list)
+    distances_list = []
+    for cat in cats_list:
+        distances_list.append(cosine(news_vector, cat))
+    print(sorted(distances_list))
+    return min(distances_list)
+
+
+def news2vector(news: News) -> list:
+    vector_len = Word.objects.last()
+    vector_len = int(vector_len.pk)
+    vector = [0] * vector_len
+    kwrds = Keyword.objects.filter(news=news).all()
+    for kw in kwrds:
+        vector[int(kw.word.pk)] = kw.frequency
+    return vector
