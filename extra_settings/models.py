4be4fb865d0f182abdea04c02d74db16b927ae32
Navid <n.soleymani@pnr.ir>
2021-08-24 20:13:49 +0430

    save update

--- extra_settings/models.py
+++ extra_settings/models.py
@@ -40,37 +40,62 @@ class FileInfo(models.Model):
         null=False,
         blank=False,
     )
-    load_complate = models.BooleanField(default=False)
-    # up to which row is it fully loaded
-    which_row = models.PositiveIntegerField(default=0)
+    _load_complate = models.BooleanField(default=False)
+
+    def load_complate(self, from_which_row=None, up_to_which_row=None):
+        if not self._load_complate:
+            return self._load_complate
+        if ((from_which_row == self.from_which_row) or (from_which_row is None)) and (
+                (up_to_which_row == self.up_to_which_row) or up_to_which_row is None):
+            return self._load_complate
+        self._load_complate = False
+        return self._load_complate
+
+    from_which_row = models.PositiveIntegerField(default=0)
+    up_to_which_row = models.PositiveIntegerField(default=0)
 
 
 class File:
-    def __init__(self):
+    def __init__(self, file_name):
         self.files_path = os.path.join(settings.MEDIA_ROOT, 'files')
         if not os.path.exists(self.files_path):
             os.mkdir(self.files_path)
-        self.info = None
-
-    def save(self, file_path, file_type='news'):
-        _uuid = uuid4()
-        zpath = os.path.join(self.files_path, f'{_uuid}.zip')
-        fname = os.path.basename(file_path)
-        with ZipFile(zpath, 'w') as zfile:
-            zfile.write(file_path, fname)
-
-        file_info = FileInfo()
-        file_info.ftype = file_type
-        file_info.fname = fname
-        fformat = fname.rsplit('.')[-1]
-        file_info.fformat = fformat
-
-        file_info.fpath = zpath
-        file_info.save()
-        self.info = file_info
-
-    def load(self, file_name: str, to_be_continued=True, from_which_row=0, up_to_which_row=None):
         self.info = FileInfo.objects.filter(fname=file_name).last()
+
+    def is_complate(self, from_which_row=None, up_to_which_row=None):
+        if self.info is None:
+            return False
+        return self.info.load_complate(from_which_row=from_which_row, up_to_which_row=up_to_which_row)
+
+    def save(self, file_path=None, file_type='news', complate=None, from_which_row=None, up_to_which_row=None):
+        if file_path is not None:
+            _uuid = uuid4()
+            zpath = os.path.join(self.files_path, f'{_uuid}.zip')
+            fname = os.path.basename(file_path)
+            with ZipFile(zpath, 'w') as zfile:
+                zfile.write(file_path, fname)
+
+            file_info = FileInfo()
+            file_info.ftype = file_type
+            file_info.fname = fname
+            fformat = fname.rsplit('.')[-1]
+            file_info.fformat = fformat
+
+            file_info.fpath = zpath
+            file_info.save()
+            self.info = file_info
+            return True
+        if complate is not None:
+            if from_which_row is not None:
+                self.info.from_which_row = from_which_row
+            if up_to_which_row is not None:
+                self.info.up_to_which_row = up_to_which_row
+            self.info._load_complate = complate
+            self.info.save()
+            return True
+        return False
+
+    def load(self, to_be_continued=True, from_which_row=0, up_to_which_row=None):
         if self.info is None:
             return []
 
@@ -87,8 +112,8 @@ class File:
             file = load_workbook(file_path, read_only=True)
             afile = file.active
             if to_be_continued:
-                if self.info.load_complate:
-                    return []
+                # if self.info.load_complate:
+                #     return []
                 from_which_row = self.info.which_row
             _data = []
             first = True
