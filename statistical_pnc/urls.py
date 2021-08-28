33458d034d8a4055522b3dc00d6b086d186ec9d2
Navid <n.soleymani@pnr.ir>
2021-08-28 20:45:10 +0430

    create feedback path

--- statistical_pnc/urls.py
+++ statistical_pnc/urls.py
@@ -1,4 +1,4 @@
-from .views import index, sample, classification, preprocessing
+from .views import index, sample, classification, preprocessing, feedback
 from django.urls import path, include
 
 app_name = 'statistical_pnc'
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', index, name='index'),
     path('sample', sample, name='sample'),
     path('classification', classification, name='classification'),
+    path('feedback', feedback, name='feedback'),
     path('preprocessing', preprocessing, name='preprocessing'),
 ]
