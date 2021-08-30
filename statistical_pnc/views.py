427788166d8bb5f90da3a912578be2c3f357940f
Navid <n.soleymani@pnr.ir>
2021-08-30 20:04:03 +0430

    classification and feedback views

--- statistical_pnc/views.py
+++ statistical_pnc/views.py
@@ -1,76 +1,23 @@
 from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render
-from django.conf import settings
-import redis
-import pickle
-from pathlib import Path
-
-from .controller import news_classification, prerequisites, NewsClassification
-from .models import reference2db, categories_list
-from .dataset2database import add2database
 
 
 def index(request: HttpRequest):
-    categories_list_name = categories_list()
+    from .models import categories_list
     return render(
         request,
         'ai_index.html',
-        context={'categories_list_name': categories_list_name},
+        context={'categories_list_name': categories_list()},
     )
-
-
-# def prerequisites(request: HttpRequest):
-#     # logging.info('Started storing dataset in the database.')
-#     # file_name = Path('staticfiles', 'HamshahriData.xlsx')
-#     # add2database(file_name)
-#     # logging.info('Data storage in the database is complete.')
-#     # logging.info('Started classification analys.')
-#     # clss = classification()
-#     # logging.info('Classification analys complate.')
-#     # from statistical_pnc.models import news2db
-#     # file_name = Path('staticfiles', 'HamshahriData.xlsx')
-#     # add2database(file_name)
-#     # s = news2db(titr_string=titr_string, content_string=content_string, category_title=category_title)
-#     return render(
-#         request,
-#         'ai_index.html',
-#         context={'word2vec_word_embeding__svm__accuracy_score': 1},  # clss,
-#     )
 
 
 def sample(request: HttpRequest):
     return render(request, 'index.html', context={})
 
 
-def classification(request: HttpRequest):
-    response = {}
-    content = request.POST['content_text_for_classify']
-    titr = request.POST['titr_text_for_classify']
-
-    if content is None or len(content) == 0:
-        response['TEXT'] = False
-        response['ERROR_MESSAGE'] = 'Please enter the news you want to categorize in this section.'
-        return JsonResponse(response)
-
-    response['TEXT'] = True
-    cn = NewsClassification()
-    category = cn.classification(content, titr)
-    # category = news_classification(reference=reference, content=content, titr=titr)
-    response['CATEGORY_TITLE'] = category.title
-    response['CATEGORY_PK'] = category.pk
-    response['NEWS_PK'] = cn.news_for_classification.pk
-    return JsonResponse(response)
-
-
-def feedback(request: HttpRequest):
-    print(request.POST['category_radios'])
-    return JsonResponse({})
-
-
 def preprocessing(request: HttpRequest):
+    from .controller import prerequisites
     prerequisites()
-    from .controller import NewsClassification
-    nc = NewsClassification()
     response = {
         'RESULT': True,
         'word2vec_word_embeding__svm__precision_score': 1,
@@ -99,6 +46,31 @@ def preprocessing(request: HttpRequest):
     }
     return JsonResponse(response)
 
-# 0790160000000000774260366
 
-# 09151712846
+def classification(request: HttpRequest):
+    from .controller import NewsClassification
+    response = {}
+    content = request.POST['content_text_for_classify']
+    titr = request.POST['titr_text_for_classify']
+
+    if content is None or len(content) == 0:
+        response['TEXT'] = False
+        response['ERROR_MESSAGE'] = 'Please enter the news you want to categorize in this section.'
+        return JsonResponse(response)
+    response['TEXT'] = True
+
+    cn = NewsClassification()
+    category = cn.classification(content, titr)
+
+    response['CATEGORY_TITLE'] = category.title
+    response['CATEGORY_PK'] = category.pk
+    response['NEWS_PK'] = cn.news_for_classification.pk
+
+    return JsonResponse(response)
+
+
+def feedback(request: HttpRequest):
+    from .controller import feedback as cfb
+    cfb(news_id=request.POST['news_pk'], category_id=request.POST['category_radios'])
+
+    return JsonResponse({})
