f4f84aad36806811eca0ed5e5077d0c6e85b2585
Navid <n.soleymani@pnr.ir>
2021-08-30 20:02:13 +0430

    create feedback controller

--- statistical_pnc/controller.py
+++ statistical_pnc/controller.py
@@ -2,10 +2,11 @@ import logging
 from pathlib import Path
 from scipy.spatial.distance import cosine
 from random import sample
-from .dataset2database import add2database
-from .models import Category, news2db, News, Word, Keyword, categories_list, Reference
-# from nvd.classification import Classification
+
 from nvd.converter import bag_of_word2one_hot
+
+from .dataset2database import add2database
+from .models import Category, news2db, News, Word, Keyword, categories_list, Reference, StatisticalWordCategory
 
 
 def prerequisites():
@@ -21,41 +22,23 @@ def prerequisites():
                                 up_to_which_row=up_to_which_row)
         add2database(file_name=file_name, part_of_data=res, part_of_data_header=header)
         file.save(complate=True, from_which_row=from_which_row, up_to_which_row=up_to_which_row)
-    # corpus_file_path = Path('staticfiles', 'HamshahriData.xlsx')
-    # add2database(corpus_file_path)
     logging.info('Data storage in the database is complete.')
-
-
-def news_classification(reference: Reference, content: str, titr: str = None) -> Category:
-    news = news2db(
-        content_string=content,
-        titr_string=titr,
-    )
-    news_vector = news2vector(news)
-    cats_list = categories_list(reference=reference, vector=True)
-    minimum_value = 100
-    minimum_index = ''
-    for cat_key, cat_val in cats_list.items():
-        dist = cosine(news_vector, cat_val)
-        if minimum_value > dist:
-            minimum_value = dist
-            minimum_index = cat_key
-    near_category = Category.objects.get(pk=minimum_index)
-    return near_category
-
-
-def news2vector(news: News) -> list:
-    vector_len = Word.objects.last()
-    vector_len = int(vector_len.pk)
-    vector = [0] * (vector_len + 1)
-    kwrds = Keyword.objects.filter(news=news).all()
-    for kw in kwrds:
-        vector[int(kw.word.pk)] = kw.frequency
-    return vector
 
 
 class NewsClassification:
     def __init__(self):
+        pass
+
+    categories_list = None
+    categories_list_pk = None
+
+    def _clist(self):
+        self.categories_list = categories_list(vector=True)
+        self.categories_list_pk = self.categories_list.keys()
+
+    news_for_classification = None
+
+    def performance(self):
         data = News.objects.filter(category__isnull=False).all()
         data_len = len(data) - 1
         data_test_len = round(data_len / 6)
@@ -63,44 +46,10 @@ class NewsClassification:
         _data = []
         for i in test_ary_list:
             _data.append(data[i])
-        self.data_for_test = _data
-        self.categories_list = None
-        self.categories_list_pk = None
-
-    def _clist(self):
-        self.categories_list = categories_list(vector=True)
-        self.categories_list_pk = self.categories_list.keys()
-
-    def _classification(self, news):
-        news_vector = news.vector
-        self._clist()
-        cats_list = self.categories_list
-        minimum_value = 100
-        minimum_index = ''
-        for cat_key, cat_val in cats_list.items():
-            dist = cosine(news_vector, cat_val)
-            if minimum_value > dist:
-                minimum_value = dist
-                minimum_index = cat_key
-        near_category = Category.objects.get(pk=minimum_index)
-        news.category = near_category
-        news.save()
-        return near_category
-    news_for_classification = None
-
-
-    def classification(self, content: str, titr: str = None) -> Category:
-        news = news2db(
-            content_string=content,
-            titr_string=titr,
-        )
-        self.news_for_classification = news
-        return self._classification(news)
-
-    def performance(self):
+        data_for_test = _data
         y_test = []
         predicted = []
-        for itm in self.data_for_test:
+        for itm in data_for_test:
             y_test.append(itm.category.pk)
             predicted.append(self._classification(itm).pk)
         from nvd.measure import true_or_false, precision, recall, accuracy
@@ -111,8 +60,59 @@ class NewsClassification:
         _accuracy = accuracy(false_negative, true_positive, true_negative, false_positive)
         return _precision, _recall, _accuracy
 
-    def feedback(self):
-        pass
+    def classification(self, content: str, titr: str = None):
+        self.news_for_classification = news2db(
+            content_string=content,
+            titr_string=titr,
+        )
+        if self.news_for_classification.category is not None:
+            return self.news_for_classification.category
+        news = self.news_for_classification.vector
+        len_news = len(news)
+        sswcs = StatisticalWordCategory.objects.all()
+        categories = Category.objects.all()
 
-    def print_d(self):
-        print(self.performance())
+        maximum = -1
+        top_cat = None
+        for cat in categories:
+            c_swcs = sswcs.filter(docs_frequency_stdev__gt=0).filter(category=cat).all()
+            cat_score = 0
+            w_c = 1
+            for swc in c_swcs:
+                if swc.word_id <= len_news:
+                    w_c += 1
+                    dist = self._dist(
+                        swc.docs_frequency_mean,
+                        news[swc.word_id],
+                        swc.docs_frequency_stdev
+                    )
+                    cat_score += dist
+            if cat_score > maximum:
+                maximum = cat_score
+                top_cat = cat
+        return top_cat
+
+    @staticmethod
+    def _dist(a, b, variance):
+        if a - variance < b < a + variance:
+            return 1
+        return 0
+
+
+def feedback(news_id: int, category_id: int) -> News:
+    from .models import statistical_word_category2db, word2db
+    from nvd.pre_processing import tokenizer
+    from nvd.extractor import Keywords
+    news = News.objects.filter(pk=news_id).first()
+    if news is None:
+        return None
+    category = Category.objects.filter(pk=category_id).first()
+    if category is None:
+        return None
+    doc = tokenizer(news.string)
+    kwrds = Keywords(fre=True)
+    keywords = kwrds.by_frequency(document=doc, stopword=1, sents=1)
+    for keyword in keywords:
+        word = word2db(keyword['word'])
+        statistical_word_category2db(word, category, docs_frequency=keyword['frequency'])
+    return news
