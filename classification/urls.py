b7ad8faa2e200b6c596cde8e703ecd85bdda31c0
Navid <n.soleymani@pnr.ir>
2021-09-12 15:54:02 +0430

    clean code

--- classification/urls.py
+++ classification/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-                  # path('', include('persian_news_classification.urls')),
                   path('', include('statistical_pnc.urls')),
                   path('sample', views.home_page_view, name='sample'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
