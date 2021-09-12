4e8e5789e52c7d341fbe79c24965eb96c876b1d7
Navid <n.soleymani@pnr.ir>
2021-09-12 16:05:15 +0430

    edit all views

--- statistical_pnc/views.py
+++ statistical_pnc/views.py
@@ -1,76 +1,30 @@
 from django.http import HttpRequest, JsonResponse
 from django.shortcuts import render
 
+from .controller import *
 
-def index(request: HttpRequest):
-    from .models import categories_list
+
+def index_view(request: HttpRequest):
+    res = index_default_data()
     return render(
         request,
         'ai_index.html',
-        context={'categories_list_name': categories_list()},
+        context=res,
     )
 
 
-def sample(request: HttpRequest):
-    return render(request, 'index.html', context={})
+def preprocessing_view(request: HttpRequest):
+    res = prerequisites()
+    return JsonResponse(res)
 
 
-def preprocessing(request: HttpRequest):
-    from .controller import prerequisites
-    prerequisites()
-    response = {
-        'RESULT': True,
-        'word2vec_word_embeding__svm__precision_score': 1,
-        'word2vec_word_embeding__svm__recall_score': 2,
-        'word2vec_word_embeding__svm__accuracy_score': 3,
-
-        'word2vec_word_embeding__mlp__precision_score': 4,
-        'word2vec_word_embeding__mlp__recall_score': 5,
-        'word2vec_word_embeding__mlp__accuracy_score': 6,
-
-        'word2vec_word_embeding__mnb__precision_score': 7,
-        'word2vec_word_embeding__mnb__recall_score': 8,
-        'word2vec_word_embeding__mnb__accuracy_score': 9,
-
-        'one_hot_encoding_word_embeding__svm__precision_score': 10,
-        'one_hot_encoding_word_embeding__svm__recall_score': 11,
-        'one_hot_encoding_word_embeding__svm__accuracy_score': 12,
-
-        'one_hot_encoding_word_embeding__mlp__precision_score': 13,
-        'one_hot_encoding_word_embeding__mlp__recall_score': 14,
-        'one_hot_encoding_word_embeding__mlp__accuracy_score': 15,
-
-        'one_hot_encoding_word_embeding__mnb__precision_score': 16,
-        'one_hot_encoding_word_embeding__mnb__recall_score': 17,
-        'one_hot_encoding_word_embeding__mnb__accuracy_score': 18,
-    }
-    return JsonResponse(response)
-
-
-def classification(request: HttpRequest):
-    from .controller import NewsClassification
-    response = {}
+def classification_view(request: HttpRequest):
     content = request.POST['content_text_for_classify']
     titr = request.POST['titr_text_for_classify']
-
-    if content is None or len(content) == 0:
-        response['TEXT'] = False
-        response['ERROR_MESSAGE'] = 'Please enter the news you want to categorize in this section.'
-        return JsonResponse(response)
-    response['TEXT'] = True
-
-    cn = NewsClassification()
-    category = cn.classification(content, titr)
-
-    response['CATEGORY_TITLE'] = category.title
-    response['CATEGORY_PK'] = category.pk
-    response['NEWS_PK'] = cn.news_for_classification.pk
-
-    return JsonResponse(response)
+    res = classification_result(content, titr)
+    return JsonResponse(res)
 
 
-def feedback(request: HttpRequest):
-    from .controller import feedback as cfb
-    cfb(news_id=request.POST['news_pk'], category_id=request.POST['category_radios'])
-
+def classification_feedback_view(request: HttpRequest):
+    classification_feedback_view(news_id=request.POST['news_pk'], category_id=request.POST['category_radios'])
     return JsonResponse({})
