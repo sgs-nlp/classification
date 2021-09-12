9aaf7d89106183941db775e9c73f18ed4fef52d4
Navid <n.soleymani@pnr.ir>
2021-09-12 20:40:34 +0430

    add applications route

--- classification/urls.py
+++ classification/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-                  path('', include('statistical_pnc.urls')),
-                  path('sample', views.home_page_view, name='sample'),
+                  path('applications/', include('statistical_pnc.urls')),
+                  path('', views.home_page_view, name='home'),
+    path('sample', views.sample_page_view, name='sample'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
