522fd0715862166994fc970a6dedeaaf946fa9f5
Navid <n.soleymani@pnr.ir>
2021-09-13 15:17:17 +0430

    add statics pages(about, contact) urls

--- classification/urls.py
+++ classification/urls.py
@@ -8,5 +8,7 @@ from . import views
 urlpatterns = [
                   path('applications/', include('statistical_pnc.urls')),
                   path('', views.home_page_view, name='home'),
-    path('sample', views.sample_page_view, name='sample'),
+                  path('about-me', views.about_me_page_view, name='about_me'),
+                  path('contact-me', views.contact_me_page_view, name='contact_me'),
+                  path('sample', views.sample_page_view, name='sample'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
