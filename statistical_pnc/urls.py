02bc727c6386fea87b2c687540f9dc974a4e1956
nvd <navidsoleymani@ymail.com>
2021-09-14 02:28:58 +0430

    add route

--- statistical_pnc/urls.py
+++ statistical_pnc/urls.py
@@ -4,8 +4,12 @@ from .views import *
 
 app_name = 'statistical_pnc'
 urlpatterns = [
-    path('', index_view, name='index'),
-    path('classification', classification_view, name='classification'),
-    path('classification/feedback', classification_feedback_view, name='classification_feedback'),
-    path('preprocessing', preprocessing_view, name='preprocessing'),
+    path('news-classification', classification_index_view, name='news_classification_index'),
+    path('news-classification/classification', classification_view, name='news_classification_classification'),
+    path('news-classification/feedback', classification_feedback_view, name='news_classification_feedback'),
+    path('news-classification/preprocessing', classification_preprocessing_view,
+         name='news_classification_preprocessing'),
+
+    path('keywords-extraction', keywords_extraction_view, name='keywords_extraction_index'),
+    path('text-similarity', text_similarity_view, name='text_similarity_index'),
 ]
